9a743b645b7d95bcd045317778ee82fba81d5eb1
robotmayo <robotmayo@gmail.com>
2014-09-02 13:06:14 -0400

    FUCK YEAH

--- shizzle.js
+++ shizzle.js
@@ -1,11 +1,22 @@
 (function(){
+    //fucking ie8
+    if( typeof document.getElementsByClassName !== "function"){
+        document.getElementsByClassName= function( className, nodeName ){
+        var a = [];
+        var re = new RegExp('(^| )'+className+'( |$)');
+        var els = nodeName.getElementsByTagName("*");
+        for(var i=0,j=els.length; i<j; i++)
+        if(re.test(els[i].className))a.push(els[i]);
+            return a;
+        };
+    }
     window.$ = window.Shizzle = Shizzle;
 
     function Shizzle(selector){
         if(typeof selector != 'string') return new Error("Selctor must be a string!");
         var charArray = selector.trim().split('')
         ,   tokens = tokenize(charArray)
-        console.log(tokens);
+        console.log(execute(tokens));
     }
 
     function tokenize(charArray){
@@ -39,6 +50,57 @@
             currentChar = charArray[pos];
         }
         return tokens;
+    }
+
+    function execute(tokens){
+        var usedId = false
+        ,   skip = false
+        ,   token
+        ,   currentEls
+        ,   returnSet = [];
+        for(var i = 0, l = tokens.length; i < l; i++){
+            token = tokens[i];
+            if( token.type === "id" && usedId) skip = true;
+            if(!skip){
+                if(token.type === "id"){
+                    // No need to do any checks as ids are always pushed to the front and executed first
+                    currentEls = document.getElementById(token.value);
+                    if(!currentEls) return [];
+                    returnSet.push(currentEls);
+                }
+                if(token.type === "element"){
+                    if(usedId){
+                        if(currentEls.tagName !== token.value.toUpperCase()) return [];
+                    }else{
+                        if(returnSet.length > 0){
+                            currentEls = returnSet;
+                            returnSet = [];
+                            for(var j = 0; j < currentEls.length; j++){
+                                if(currentEls[j].tagName === token.value.toUpperCase()) returnSet.push(currentEls[j])
+                            }
+                        }else{
+                            console.log(token.value)
+                            returnSet.push.apply(returnSet, Array.prototype.slice.call(document.getElementsByTagName(token.value)));
+                        }
+                    }
+                }else if(token.type === "class"){
+                    if(usedId){
+                        if(currentEls.className.indexOf(token.value === -1)) return [];
+                    }else{
+                        if(returnSet.length > 0){
+                            currentEls = returnSet;
+                            returnSet = [];
+                            for(j = 0; j < currentEls.length; j++){
+                                if(currentEls[j].className.indexOf(token.value) > -1) returnSet.push(currentEls[j]);
+                            }
+                        }else{
+                            returnSet.push.apply(returnSet, Array.prototype.slice.call(document.getElementsByClassName(token.value)));
+                        }
+                    }
+                }
+            }
+        }
+        return returnSet;
     }
 
     function token(type, value){
